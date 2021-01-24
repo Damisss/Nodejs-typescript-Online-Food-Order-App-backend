b4168970b0be51e5ead434d247d7191c7e60b75e
Adama <samake8@gmail.com>
2021-01-24 23:53:04 +0100

    remove mongodb path

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const mongooseUrl = 'mongodb://Adama:Adam1234@cluster0-shard-00-00.5po2q.mongodb.net:27017,cluster0-shard-00-01.5po2q.mongodb.net:27017,cluster0-shard-00-02.5po2q.mongodb.net:27017/foodOrder?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
-export const MONGODB_TEST_URL = 'mongodb://Adama:Adam1234@cluster0-shard-00-00.5po2q.mongodb.net:27017,cluster0-shard-00-01.5po2q.mongodb.net:27017,cluster0-shard-00-02.5po2q.mongodb.net:27017/foodOrder_test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+export const mongooseUrl = 'mongodb path'
+export const MONGODB_TEST_URL = 'mongodb path'
 export const APP_SECRET_KEY = 'secretKey'
